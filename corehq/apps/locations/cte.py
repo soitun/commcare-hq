96460edb639455bfcb1f766dfca89c24456a9e2a
Daniel Miller <dmiller@dimagi.com>
2018-01-24 10:38:10 -0500

    Improve cte docs and simplify naming logic

--- corehq/apps/locations/cte.py
+++ corehq/apps/locations/cte.py
@@ -17,49 +17,48 @@ from django.db.models.sql.compiler import (
 
 
 class With(object):
-    """Common Table Expression primitive for Django
+    """Common Table Expression query object: `WITH ...`
+
+    :param queryset: A queryset to use as the body of the CTE.
+    :param name: Optional name parameter for the CTE (default: "cte").
+    This must be a unique name that does not conflict with other
+    entities (tables, views, functions, other CTE(s), etc.) referenced
+    in the given query as well as in the query to which this CTE will
+    eventually be added.
     """
 
     def __init__(self, queryset, name="cte"):
         self._queryset = queryset
         self.name = name
         self.col = CTEColumns(self)
-        # Purposely overwrite recursive classmethod with boolean value
-        # since the method is not useful on instances, and the attribute
-        # is used to tell if the CTE is recursive or not.
-        self.recursive = False
-        self.alias = None
+
+    def __repr__(self):
+        return "<With {}>".format(self.name)
 
     @classmethod
     def recursive(cls, make_cte_queryset, name="cte"):
-        """Create a recursive CTE
-
-        This method is overwritten with a boolean value on CTE
-        instances. The instance attribute indicates if the CTE is
-        recursive or not. In other words
-        ``callable(With.recursive) != callable(With(qs).recursive)``.
+        """Recursive Common Table Expression: `WITH RECURSIVE ...`
 
         :param make_cte_queryset: Function taking a single argument (a
         not-yet-fully-constructed cte object) and returning a `QuerySet`
         object. The returned `QuerySet` normally consists of an initial
         statement unioned with a recursive statement.
+        :param name: See `name` parameter of `__init__`.
         :returns: The fully constructed recursive cte object.
         """
         cte = cls(None, name)
         cte._queryset = make_cte_queryset(cte)
-        cte.recursive = True
         return cte
 
     def _add_to_query(self, query):
         if not query.tables:
             # HACK? prevent CTE becoming the initial alias
             query.get_initial_alias()
-        num = len(query._cte_refs)
-        while True:
-            name = "{}{}".format(self.name, num)
-            if name not in query.tables:
-                break
-            num += 1
+        name = self.name
+        if name in query.tables:
+            raise ValueError("cannot add CTE with name '%s' because "
+                "an entity with that name is already referenced in this "
+                "query's FROM clause" % name)
         query.add_extra(
             select=None,
             select_params=None,
@@ -68,25 +67,6 @@ class With(object):
             tables=[name],
             order_by=None,
         )
-        alias = name
-        query._cte_refs[self] = (name, alias)
-        return name, alias
-
-    def _get_name_alias(self, query):
-        """Get `(name, alias)` for this CTE in the given query
-
-        This adds the CTE to the query's extra tables if it is not
-        already reference by the query.
-        """
-        if isinstance(query.model, CTEModel):
-            cte = query.model._meta._cte()
-            assert cte is self, (cte, self)
-            assert query is not self._queryset.query
-            return self._get_name_alias(self._queryset.query)
-        try:
-            return query._cte_refs[self]
-        except KeyError:
-            return self._add_to_query(query)
 
     def _resolve_ref(self, name):
         return self._queryset.query.resolve_ref(name)
@@ -95,9 +75,9 @@ class With(object):
         """Join this CTE to the given model or queryset
 
         This CTE will be refernced by the returned queryset, but the
-        corresponding `WITH ...` statement will not be prepended
-        to the queryset's SQL output; use `cte.queryset()` or
-        `CTEQuerySet.with_cte(cte)` to achieve that outcome.
+        corresponding `WITH ...` statement will not be prepended to the
+        queryset's SQL output; use `<CTEQuerySet>.with_cte(cte)` to
+        achieve that outcome.
 
         :param model_or_queryset: Model class or queryset to which the
         CTE should be joined.
@@ -120,8 +100,10 @@ class With(object):
     def queryset(self, model=None):
         """Get a queryset selecting from this CTE
 
-        The CTE `WITH ...` clause will be prepended to the queryset's
-        SQL output.
+        This CTE will be refernced by the returned queryset, but the
+        corresponding `WITH ...` statement will not be prepended to the
+        queryset's SQL output; use `<CTEQuerySet>.with_cte(cte)` to
+        achieve that outcome.
 
         :param model: Optional model class to use as the queryset's
         primary model. If this is provided the CTE will be added to
@@ -132,7 +114,8 @@ class With(object):
         query = CTEQuery(model)
         if model is None:
             model = query.model = CTEModel(self, query)
-        query._with_ctes.append(self)
+        else:
+            self._add_to_query(query)
         return CTEQuerySet(model, query)
 
 
@@ -157,7 +140,7 @@ class CTEMeta(Options):
 
     @property
     def db_table(self):
-        return self._cte()._get_name_alias(self._query())[0]
+        return self._cte().name
 
     @db_table.setter
     def db_table(self, value):
@@ -203,18 +186,23 @@ class CTEColumn(Expression):
 
     @property
     def output_field(self):
+        if self.cte._queryset is None:
+            raise ValueError(
+                "cannot resolve '{cte}.{name}' output field in recursive CTE "
+                "setup. Hint: use ExpressionWrapper({cte}.col.{name}, "
+                "output_field=...)".format(cte=self.cte.name, name=self.name)
+            )
         return self._ref().output_field
 
     def as_sql(self, compiler, connection):
         qn = compiler.quote_name_unless_alias
-        alias = self.cte._get_name_alias(compiler.query)[1]
         ref = self._ref()
         if isinstance(ref, Col):
             # resolve column references like 'pk'
             column = ref.target.column
         else:
             column = self.name
-        return "%s.%s" % (qn(alias), qn(column)), []
+        return "%s.%s" % (qn(self.cte.name), qn(column)), []
 
 
 class LazyReentrantGenerator(object):
@@ -244,13 +232,13 @@ class CTEQuerySet(QuerySet):
         super(CTEQuerySet, self).__init__(model, query, using, hints)
 
     def with_cte(self, cte):
-        """Add a Common Table Expression to the given queryset
+        """Add a Common Table Expression to this queryset
 
         The CTE `WITH ...` clause will be prepended to the queryset's
         SQL output so it can be referenced in annotations, filters, etc.
         """
         qs = self._clone()
-        qs.query._with_ctes.append(cte)
+        qs.query._with_ctes.insert(0, cte)
         return qs
 
     def aggregate(self, *args, **kwargs):
@@ -267,7 +255,6 @@ class CTEQuery(Query):
     def __init__(self, *args, **kwargs):
         super(CTEQuery, self).__init__(*args, **kwargs)
         self._with_ctes = []
-        self._cte_refs = {}
 
     def get_compiler(self, using=None, connection=None):
         """ Overrides the Query method get_compiler in order to return
@@ -292,7 +279,6 @@ class CTEQuery(Query):
         if isinstance(clone.model, CTEModel):
             clone.model = clone.model._copy_for_query(clone)
         clone._with_ctes = self._with_ctes[:]
-        clone._cte_refs = self._cte_refs.copy()
         return clone
 
     if django.VERSION < (2, 0):
@@ -316,28 +302,28 @@ class CTEQuery(Query):
 
 class CTECompiler(object):
 
-    CTE = "WITH {name} AS ({query}) "
-    RECURSIVE = "WITH RECURSIVE {name} AS ({query}) "
+    TEMPLATE = "{name} AS ({query})"
 
     @classmethod
     def generate_sql(cls, connection, query, as_sql):
         if query.combinator:
             return as_sql()
 
-        sql = []
+        ctes = []
         params = []
         for cte in query._with_ctes:
             compiler = cte._queryset.query.get_compiler(connection=connection)
             cte_sql, cte_params = compiler.as_sql()
-            cte_name = cte._get_name_alias(query)[0]
-            temp = cls.RECURSIVE if cte.recursive else cls.CTE
-            sql.append(temp.format(name=cte_name, query=cte_sql))
+            ctes.append(cls.TEMPLATE.format(name=cte.name, query=cte_sql))
             params.extend(cte_params)
 
+        # Always use WITH RECURSIVE
+        # https://www.postgresql.org/message-id/13122.1339829536%40sss.pgh.pa.us
+        sql = ["WITH RECURSIVE", ", ".join(ctes)] if ctes else []
         base_sql, base_params = as_sql()
         sql.append(base_sql)
         params.extend(base_params)
-        return "".join(sql), tuple(params)
+        return " ".join(sql), tuple(params)
 
 
 class CTEUpdateQuery(UpdateQuery, CTEQuery):
